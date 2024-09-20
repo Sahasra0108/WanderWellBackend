eede486761d05f1f231144c8ccd2451456e675a6
Sachini Sahasra <98446017+Sahasra0108@users.noreply.github.com>
2024-09-20 16:22:03 +0530

    initial commit

---
+++ Wanderwell-backend/src/main/java/com/WanderWell/Wanderwell_backend/WanderwellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.WanderWell.Wanderwell_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderwellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderwellBackendApplication.class, args);
+	}
+
+}
---
+++ Wanderwell-backend/src/test/java/com/WanderWell/Wanderwell_backend/WanderwellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WanderWell.Wanderwell_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderwellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
